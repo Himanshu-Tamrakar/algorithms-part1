32ab1349ebc5eba048a63d8db5b1925b8e689b1d
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2025-10-11 12:18:11 +0530

    8puzzle problem solution with un solvable puzzle problem

--- src/PriorityQueues/EightPuzzle/Solver.java
+++ src/PriorityQueues/EightPuzzle/Solver.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Solver {
     private SearchNode lastNode;
     private boolean solvable;
-    private int moves = 0;
+    private int minMoves = 0;
 
 
     // find a solution to the initial board (using the A* algorithm)
@@ -19,42 +19,73 @@ public class Solver {
         if (initial == null)
             throw new java.lang.IllegalArgumentException();
 
-        MinPQ<SearchNode> searchNodes = new MinPQ<>();
-        searchNodes.insert(new SearchNode(initial, 0, null));
+        int moves = 0;
+        int twinMoves = 0;
 
-        Queue<Board> neighbourBoards = new Queue<>();
-        Set<Board> visited = new HashSet<>();
+        Queue<Board> neighbors = new Queue<Board>();
+        Queue<Board> twinNeighbors = new Queue<Board>();
 
-        while (!searchNodes.isEmpty() && !solvable) {
-            SearchNode currentSearchNode = searchNodes.delMin();
-            Board currentBoard = currentSearchNode.getBoard();
+        MinPQ<SearchNode> searchNodes = new MinPQ<SearchNode>();
+        MinPQ<SearchNode> twinNodes = new MinPQ<SearchNode>();
 
-            if (currentBoard.isGoal()) {
-                solvable = true;
-                lastNode = currentSearchNode;
-                return;
+        SearchNode searchNode = new SearchNode(initial, moves, null);
+        SearchNode twinSearchNode = new SearchNode(initial.twin(), twinMoves, null);
+
+        twinNodes.insert(twinSearchNode);
+        searchNodes.insert(searchNode);
+
+        boolean solved = false;
+        boolean twinSolved = false;
+        //System.out.println("Get Board: " + searchNodes.delMin().getBoard());
+        SearchNode current = null;
+
+        while (!solved && !twinSolved) {
+            current = searchNodes.delMin();
+            SearchNode predecessor = current.getPredecessor();
+            Board temp = current.getBoard();
+            solved = temp.isGoal();
+
+            SearchNode twinCurrent = twinNodes.delMin();
+            SearchNode twinPredecessor = twinCurrent.getPredecessor();
+            Board twinTemp = twinCurrent.getBoard();
+            twinSolved = twinTemp.isGoal();
+
+            for (Board b : temp.neighbors())
+                neighbors.enqueue(b);
+
+            for (Board b : twinTemp.neighbors())
+                twinNeighbors.enqueue(b);
+
+            while(neighbors.size() > 0) {
+                Board board = neighbors.dequeue();
+                int move = current.getMoves();
+                move++;
+                if (predecessor != null && predecessor.getBoard().equals(board))
+                    continue;
+
+                SearchNode neighborNode = new SearchNode(board, move, current);
+                //System.out.println("Priorities " + neighborNode.getPriority());
+                searchNodes.insert(neighborNode);
             }
 
-            if (visited.contains(currentBoard)) {
-                continue;
-            }
-            visited.add(currentBoard);
+            while(twinNeighbors.size() > 0) {
+                Board board = twinNeighbors.dequeue();
+                int twinMove = current.getMoves();
+                twinMove++;
+                if (twinPredecessor != null && twinPredecessor.getBoard().equals(board))
+                    continue;
 
-            for (Board neighborBoard : currentBoard.neighbors()) {
-                // This is optimization
-                if (neighborBoard.isGoal()) {
-                    solvable = true;
-                    lastNode = new SearchNode(neighborBoard, currentSearchNode.getMoves() + 1, currentSearchNode);
-                    break;
-                }
-                // Check against predecessor and visited set
-                if (!visited.contains(neighborBoard)) {
-                    SearchNode neighborNode = new SearchNode(neighborBoard, currentSearchNode.getMoves() + 1, currentSearchNode);
-                    searchNodes.insert(neighborNode);
-                }
+                SearchNode neighborNode = new SearchNode(board, twinMove, twinCurrent);
+                twinNodes.insert(neighborNode);
             }
+
+            moves = current.getMoves() + 1;
+            twinMoves = twinCurrent.getMoves() + 1;
+            lastNode = current;
         }
-        moves = lastNode.getMoves();
+
+        solvable = !twinSolved;
+        minMoves = moves - 1;
     }
 
     // is the initial board solvable?
@@ -66,7 +97,7 @@ public class Solver {
     public int moves() {
         if (!isSolvable())
             return -1;
-        return moves;
+        return minMoves;
 
     }
 
@@ -122,19 +153,26 @@ public class Solver {
     }
     // test client (see below)
     public static void main(String[] args) {
+//        int[][] puzzle = {
+//                {0,  1,  3},
+//                {4,  2,  5},
+//                {7,  8,  6}
+//        };
+
         int[][] puzzle = {
-                {0,  1,  3},
-                {4,  2,  5},
-                {7,  8,  6}
+                {1,  2,  3},
+                {4,  5,  6},
+                {8,  7,  0}
+
         };
 
         Board board = new Board(puzzle);
 
         Solver solver = new Solver(board);
-        for (Board solution: solver.solution()) {
-            StdOut.println(solution);
-        }
-
+//        for (Board solution: solver.solution()) {
+//            StdOut.println(solution);
+//        }
+        StdOut.println("Can Solve: " + solver.isSolvable());
         StdOut.println("Moves: " + solver.moves());
     }
 
