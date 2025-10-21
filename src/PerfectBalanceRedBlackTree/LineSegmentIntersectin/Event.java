74d4d4f69b3111e1091e111f054acc5a1491e86d
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2025-10-21 16:44:36 +0530

    Orthogonal Line Segment Algorithms.

--- src/PerfectBalanceRedBlackTree/LineSegmentIntersectin/Event.java
+++ src/PerfectBalanceRedBlackTree/LineSegmentIntersectin/Event.java
@@ -45,10 +45,6 @@ public class Event implements Comparable<Event> {
         return 0;
     }
 
-
-
-
-
     @Override
     public String toString() {
         return String.format("Event(%d, %d, %s, %s)", x, y, segment, type);
